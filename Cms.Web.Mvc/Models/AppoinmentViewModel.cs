3c13bdf358ba8bcfab321aec0b3ce5139fc530f4
keremgencer <keremgencer193@gmail.com>
2023-08-07 01:17:18 +0300

    Footer ve appoinment sectionları dinamik hale getirildi.

---
+++ Cms.Web.Mvc/Models/AppoinmentViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Cms.Business.Dtos;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Cms.Web.Mvc.Models
+{
+	public class AppoinmentViewModel
+	{
+		public IEnumerable<DoctorSelectListModel> Doctors { get; set; }
+		public IEnumerable<SelectListItem> Departments { get; set; }
+	}
+}
---
+++ Cms.Web.Mvc/ViewComponents/AppoinmentViewComponent.cs
@@ -0,0 +1,33 @@
+﻿using Cms.Business.Services.Abstract;
+using Cms.Web.Mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Text.RegularExpressions;
+
+namespace Cms.Web.Mvc.ViewComponents
+{
+	public class AppoinmentViewComponent : ViewComponent
+	{
+		private readonly IDepartmentService _departmentService;
+		private readonly IDoctorService _doctorService;
+		public AppoinmentViewComponent(IDepartmentService departmentService, IDoctorService doctorService)
+		{
+			_departmentService = departmentService;
+			_doctorService = doctorService;
+		}
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var doctors = _doctorService.GetAll();
+			return View(new AppoinmentViewModel {
+				Departments = _departmentService
+				.GetAll()
+				.Select(e => new SelectListItem { Value = Convert.ToString(e.Id), Text = e.Name })
+				.ToList()
+
+				, Doctors = doctors
+				.Select(e => new DoctorSelectListModel { Value = Convert.ToString(e.Id), Text = e.Name + " " + e.Surname ,DepartmentId = Convert.ToString(e.DepartmentDtoId)})
+				.ToList()
+			});
+		}
+	}
+}
