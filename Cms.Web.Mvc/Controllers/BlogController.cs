22a3d5d89dc0e4221211cb4515d83307b481491f
keremgencer <keremgencer193@gmail.com>
2023-08-22 02:27:41 +0300

    Blog sistemi temeli atıldı.

---
+++ Cms.Web.Mvc/Controllers/BlogController.cs
@@ -0,0 +1,33 @@
+﻿using Cms.Business.Dtos;
+using Cms.Business.Services.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata;
+
+namespace Cms.Web.Mvc.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly IPostService _postService;
+
+        public BlogController(IPostService postService)
+        {
+            _postService= postService;
+        }
+
+        public IActionResult Index([FromQuery]int page = 1)
+        {
+            return View(_postService.GetAll(page = page));
+        }
+
+        //todo Blog indexdeki sayfalar < 1 2 3 > ile değişebilmeli
+        //todo Departman ve tag ile post filtreleme eklenecek
+        //todo comment ekleme eklenmeli
+
+        public IActionResult Detail(int id)
+        {
+            var e = _postService.GetById(id);
+
+			return View(e);
+        }
+    }
+}
