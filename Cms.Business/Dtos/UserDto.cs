c75608f21bddd64b64b1bce6ef81082d6adc688f
Mustafa Kaan Sevinç <mksmus86@gmail.com>
2023-09-10 15:52:12 +0300

    UserDto tarafına validasyon özellikleri eklendi | Blog ındex sayfasına başlık ve creat butonu eklendi | sayfaların ındex ve creat sayfalarında türkçeleştirme buton ekleme ve style işlemleri yapıldı | Layout Test adındaki Klasör silindi |

--- Cms.Business/Dtos/UserDto.cs
+++ Cms.Business/Dtos/UserDto.cs
@@ -5,12 +5,28 @@ namespace Cms.Business.Dtos
 {
     public class UserDto : BaseDto
     {
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string Name { get; set; }
-        public string Surname { get; set; }
-        public string City { get; set; }
-        public string Phone { get; set; }
+		[Required(ErrorMessage = "E-posta adresi zorunludur.")]
+		[EmailAddress(ErrorMessage = "Geçersiz e-posta adresi.")]
+		public string Email { get; set; }
 
-    }
+		[Required(ErrorMessage = "Şifre zorunludur.")]
+		[MinLength(6, ErrorMessage = "Şifre en az 6 karakter olmalıdır.")]
+		public string Password { get; set; }
+
+		[Required(ErrorMessage = "Ad alanı zorunludur.")]
+		[MaxLength(20, ErrorMessage = "Ad alanı 20 karakteri geçemez.")]
+		public string Name { get; set; }
+
+		[Required(ErrorMessage = "Soyad alanı zorunludur.")]
+		[MaxLength(20, ErrorMessage = "Soyad alanı 20 karakteri geçemez.")]
+		public string Surname { get; set; }
+
+		[Required(ErrorMessage = "Şehir zorunludur.")]
+		public string City { get; set; }
+
+		[Required(ErrorMessage = "Telefon numarası zorunludur.")]
+		[RegularExpression(@"^\d{10}$", ErrorMessage = "Geçersiz telefon numarası. Lütfen 10 haneli bir telefon numarası girin.")]
+		public string Phone { get; set; }
+
+	}
 }
